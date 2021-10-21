f4f45b3e9b23505921c9e5a235965f3c7eef6f4f
udeeunice <udeeunicekalu@gmail.com>
2021-10-21 12:28:17 +0100

    weekly deals without the 10% off

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Weekly from "./components/weeklybestdeals/Weekly"
+function App() {
+  return (
+    <div>
+     
+   <Weekly/>
+     
+      
+      
+    </div>
+  );
+}
+
+export default App;
