11f59c39f2dede5994c7e95b0d140facdac5d449
Ejoor Emmanuel <ejooremmanuel@gmail.com>
2021-10-22 09:05:21 +0100

    footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import "semantic-ui-css/semantic.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import ShopBar from "./ShopBar/ShopBar";
+import Benefits from "./BenefitsBar/BenefitsBar";
+import Banner from "./BannerDisplay/BannerDisplay";
+import SubMenuBar from "./SubMenuBar/SubMenuBar";
+import MenuBar from "./Menubar/MenuBar";
+
 function App() {
   return (
-    <div>
-      <h1>Ready!</h1>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "space-around",
+      }}
+    >
+      <MenuBar />
+      <SubMenuBar />
+      <Banner />
+      <ShopBar />
+      <Benefits />
     </div>
   );
 }
---
+++ src/Footerbar/Footerbar.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Footerbar = () => {
+  return <div></div>;
+};
+
+export default Footerbar;
